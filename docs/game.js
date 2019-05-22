bebbc9e3250dc49d0715d3cdfc97f2c0d671a061
GreenNope <tlash1000@gmail.com>
2019-05-22 08:07:35 -0400

    Create game.js

---
+++ docs/game.js
@@ -0,0 +1,15 @@
+var canvas = getElementById("gameCanvas")
+var context = canvas.getContext("2d")
+var world = [[{type: "grass"}]]
+for(var y = 0; y<world.length; y++) {
+  for(var x = 0; x<world[y].length; x++) {
+    drawWithObject(x, y, world[y][x])
+  }
+}
+
+function drawWithObject (x, y, object) {
+  if(object.type == "grass") {
+    context.fillRect(x * 10, y * 10, 10, 10)
+  }
+
+}
